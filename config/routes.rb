add494f1440e1ccc2f8bb03425ea09d91605023b
sungjaek <kim.sungjae19840707@gmail.com>
2016-03-27 09:53:50 +0000

    using Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root "messages#index"
-  resources :messages
+   root "messages#index"
+   resources :messages
   
  # root 'messages#index'
  # resources :messages , only: [:create]
